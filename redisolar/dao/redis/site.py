f1bc8013d1d41037acb2c0935606ce11aa788793
James K. Pringle <jameskpringle@gmail.com>
2020-08-25 11:58:24 -0700

    Implement insert_all and include tests

--- redisolar/dao/redis/site.py
+++ redisolar/dao/redis/site.py
@@ -36,9 +36,11 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
 
     def find_all(self, **kwargs) -> Set[Site]:
         """Find all Sites in Redis."""
-        # START Challenge #1
-        # Remove this line when you've written code to build `site_hashes`.
-        site_hashes = []  # type: ignore
-        # END Challenge #1
-
+        site_ids_key = self.key_schema.site_ids_key()
+        client = kwargs.get('pipeline', self.redis)
+        site_ids = client.smembers(site_ids_key)
+        site_hashes = []
+        for site_id in site_ids:
+            hash_key = self.key_schema.site_hash_key(site_id)
+            site_hashes.append(client.hgetall(hash_key))
         return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
